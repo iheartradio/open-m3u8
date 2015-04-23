ef1e112a5a8e1f4f8fd9bc475167c46f17b395c0
Daniel Tashjian <thewopple@gmail.com>
2015-04-23 12:39:10 -0400

    Removed some undesired comments.

--- src/main/java/com/iheartradio/m3u8/PlaylistWriter.java
+++ src/main/java/com/iheartradio/m3u8/PlaylistWriter.java
@@ -5,22 +5,6 @@ import java.io.OutputStream;
 
 import com.iheartradio.m3u8.data.Playlist;
 
-/** 
- * Writing out a {@link Playlist} again works the same way as parsing via {@link PlaylistParser}:
-<pre>OutputStream outputStream = ...
-PlaylistWriter writer = new PlaylistWriter();
-writer.write(outputStream, updatedPlaylist, Format.EXT_M3U, Encoding.UTF_8);</pre>
- * <p>causing this playlist to be written:</p>
-<pre>#EXTM3U
-#EXT-X-VERSION:5
-#EXT-X-TARGETDURATION:3
-#EXT-X-MEDIA-SEQUENCE:1
-#EXTINF:3.0,Example Song
-example.mp3
-#EXTINF:3.0,Additional Song
-additional.mp3
-#EXT-X-ENDLIST</pre>
- */
 public class PlaylistWriter {
     
     public void write(OutputStream outputStream, Playlist playlist, Format format, Encoding encoding) throws IOException, ParseException {
